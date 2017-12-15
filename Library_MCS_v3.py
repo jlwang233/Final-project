90d161feeca7494a3cda8a9375860bd427332e1f
jlwang233 <jwang282@illinois.edu>
2017-12-15 05:07:22 -0600

    update class simulation

--- Library_MCS_v3.py
+++ Library_MCS_v3.py
@@ -11,7 +11,7 @@ class Seat:
 
     """
     #
-    def __init__(self, seat_id, seat_type):
+    def __init__(self, seat_id: int, seat_type: str):
         """
 
         :param seat_id: an integer containing id of seat
@@ -57,7 +57,7 @@ class Seat:
             self.size = 6
         return self.size
 
-    def add_new(self, new_study_size):
+    def add_new(self, new_study_size: int):
         """
 
         :param new_study_size: an integer containing enter study unit size
@@ -69,7 +69,7 @@ class Seat:
         """
         self.taken += new_study_size
 
-    def remove_old(self,exit_study_size):
+    def remove_old(self,exit_study_size: int):
         """
 
         :param exit_study_size: an integer containing exit study unit size
@@ -85,12 +85,6 @@ class Seat:
         >>> a.add_new(1)
         >>> a.occupied()
         True
-        >>> a = Seat(3,'desk')
-        >>> a.add_new(2)
-        >>> a.occupied()
-        Traceback (most recent call last):
-        ...
-        ValueError
         >>> a = Seat(95,'room')
         >>> a.add_new(3)
         >>> a.occupied()
@@ -122,7 +116,7 @@ class Seat:
         >>> a.left()
         3
         >>> a.add_new(1)
-        >>> a.seat_left()
+        >>> a.left()
         2
         >>> b = Seat(90,'room')
         >>> b.add_new(3)
@@ -141,10 +135,9 @@ class Seat:
                 return self.get_seat_size()
 
 
-
 class Study:
     # An OO class to create a study unit
-    def __init__(self, study_id):
+    def __init__(self, study_id: int):
         """
 
         :param study_id: an integer containing ID of the study unit
@@ -165,11 +158,11 @@ class Study:
         """
         return self.ID
 
-    def get_study_size(self):
+    def get_study_size(self) -> int:
         """
 
         :return: an integer containing the size of the study unit
-        >>> a = Study(3).get_study_size
+        >>> a = Study(3).get_study_size()
         >>> a in [1, 2, 3, 4, 5, 6]
         True
         """
@@ -258,66 +251,93 @@ class Study:
 
 
 class Simulation:
-    def __init__(self,new_study_id):
-        self.seats = []
-        self.study_record = []
-        self.exit_time = pd.DataFrame(columns=['StudyID','SeatID','StudySize','timeExit'])
-        self.new_study_id = new_study_id
+    def __init__(self,new_study_id, a, b, c):
+        """
 
-    @staticmethod
-    def seat_arrange(a, b, c):
-        """ arrange the seats in the library with sear_id and seat_type
-
+        :param new_study_id: an integer containing input of new study id
         :param a: an integer containing end number of desk
         :param b: an integer containing end number of room
         :param c: an integer containing end number of table
         """
-        seats = []
-        for i in range(a):
-            seats.append(Seat(i+1, 'desk'))
-        for i in range(a, b):
-            seats.append(Seat(i+1, 'room'))
-        for i in range(b, c):
-            seats.append(Seat(i+1, 'table'))
+        self.seats = []
+        self.study_record = []
+        self.exit_time = pd.DataFrame(columns=['StudyID','SeatID','StudySize','timeExit'])
+        self.new_study_id = new_study_id
+        self.desk = a
+        self.room = b
+        self.table = c
 
-    def study_record(self, study_id):
+    def get_study_record(self) -> list:
+        return self.study_record
+
+    def get_exit_time_record(self) -> pd.DataFrame:
+        return self.exit_time
+
+    def seat_arrange(self) -> list:
+        """ arrange the seats in the library with sear_id and seat_type
+
+        """
+        for i in range(self.desk):
+            self.seats.append(Seat(i+1, 'desk'))
+        for i in range(self.desk, self.room):
+            self.seats.append(Seat(i+1, 'room'))
+        for i in range(self.room, self.table):
+            self.seats.append(Seat(i+1, 'table'))
+
+        return self.seats
+
+    def create_study_record(self, study_id: int) -> list:
+        """
+
+        :param study_id: an integer representing study unit id
+        :return:a list containing study record
+        """
         self.study_record.append(Study(study_id))
+        return self.study_record
 
+    def seat_assign(self, study: Study, seat: Seat):
+        """
 
-    def seat_assign(self, study: Study, seat:Seat):
+        :param study: a Study Class containing the assigned study unit
+        :param seat: a Seat class containing the assigned seat unit
+        """
         study.seatAssigned = seat.seat_id
         study.timeEnter = study.time_stamp()
         seat.add_new(study.get_study_size())
         self.exit_time_record(study)
 
-
-    def study_enter(self, study:Study):
+    def study_enter(self, study: Study) -> object:
         if study.get_study_size() == 1:
-            wait_individual = [astudy for astudy in self.study_record if astudy.size == 1
-                               and astudy.seatAssigned is None]
+            wait_individual = []
+            for astudy in self.study_record:
+                if astudy.size == 1 and astudy.seatAssigned is None:
+                    wait_individual.append(astudy)
             if len(wait_individual) == 0:
                 for seat in self.seats:
                     if seat.get_seat_type() in ['desk','table']:
                         if seat.occupied() is False:
                             self.seat_assign(study,seat)
                             break
-
+            return
 
         if study.get_study_size() > 1:
-            wait_group = [astudy for astudy in self.study_record if astudy.size > 1
-                          and astudy.seatAssigned is None]
+            wait_group = []
+            for astudy in self.study_record:
+                if astudy.size > 1 and astudy.seatAssigned is None:
+                    wait_group.append(astudy)
             if len(wait_group) == 0:
                 for seat in self.seats:
                     if seat.get_seat_type() in ['room','table']:
-                        if seat.seat_left() >= study.get_study_size():
-                            self.seat_assign()
+                        if seat.left() >= study.get_study_size():
+                            self.seat_assign(study,seat)
                             break
 
     def exit_time_record(self, study: Study):
         col_name = ['StudyID', 'SeatID', 'StudySize', 'timeExit']
         new_record = pd.DataFrame([[study.get_study_id(), study.get_study_seat(), study.get_study_size(),
-                                    study.time_exit()]], columns=col_name)
-        self.exit_time = self.exit_time.append(new_record, columns= col_name)
+                                    study.time_exit()]], columns= col_name)
+        self.exit_time = self.exit_time.append(new_record, ignore_index=True)
+        self.exit_time = self.exit_time.sort_values(by=['timeExit']).reset_index(drop=True)
 
     def study_exit(self, study: Study):
         a = study.get_study_seat()
@@ -344,7 +364,38 @@ class Simulation:
                         break
 
     def simulator(self):
-        for i in range(self.new_study_id*5):
+        self.seat_arrange()
+        for i in range(1,self.new_study_id*5+1):
+            a = self.get_exit_time_record()
+            for m in range(len(a)):
+                exit_record = self.exit_time.loc[m]
+                if exit_record.timeExit == i:
+                    exit_study = self.study_record[exit_record.StudyID-1]
+                    self.study_exit(exit_study)
+
+            if i % 5 == 0:
+                self.create_study_record(i//5)
+                b = self.study_record
+                self.study_enter(b[i//5-1])
+
+        print(self.exit_time, self.new_study_id*5)
+
+        found = False
+        for study in self.study_record:
+            if study.get_study_seat() == 0:
+                print(study.get_study_id())
+                found = True
+        print(found)
+
+
+Simulation(10, 1, 2, 3).simulator()
+
+
+
+
+
+
+
 
 
 
